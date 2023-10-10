8aff5e8f81f42203719226f9f29a3b7b886e83a6
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-10-10 15:08:08 +0800

    6-consolidate-all-backframe-files-in-deepts (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import express from "express";
 import * as deep from "./utils";
 
-export const app = express();
+const app = express();
 export const port = 3222;
 app.use(express.json());
 
 app.listen(port, deep.startServer);
 
-// DO NOT CHANGE any of the codes below this line
-app.use("/deep", deep.deep);
-app.use(deep.RouteNotFoundMiddleware);
-app.use(deep.ErrorHandlerMiddleware);
-// DO NOT CHANGE any of the codes above this line
+export default app;
